fa5f40663264752b6d8d06955b1b9c3ede5e9cc4
Tyler Hevia <tylerhevia@gmail.com>
2018-01-01 16:39:57 -0500

    display scrambled word on click of Start button

--- app/components/ScrambledWord.js
+++ app/components/ScrambledWord.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { scrambleWord } from '../helpers/random';
 
 export const ScrambledWord = props => {
   const { word } = props;
+  let scrambled = scrambleWord(word);
   return (
     <p className="scrambled-word">
-      {word}
+      {scrambled}
     </p>
   );
 };
--- app/helpers/random.js
+++ app/helpers/random.js
@@ -4,4 +4,16 @@ export const selectRandomNumber = max => {
 
 export const selectRandomWord = words => {};
 
-export const scrambleWord = word => {};
+export const scrambleWord = word => {
+  word = word.split('');
+  let max = word.length;
+
+  for (let i = 0; i < word.length; i++) {
+    let randomPosition = Math.round(Math.random() * max);
+    let current = word[i];
+    word[i] = word[randomPosition];
+    word[randomPosition] = current;
+  }
+
+  return word.join('');
+};
