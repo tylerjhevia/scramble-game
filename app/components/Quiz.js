c70b89e3290f8540c4496e5dfa9b9d42d303f68c
Tyler Hevia <tylerhevia@gmail.com>
2018-01-01 16:12:02 -0500

    create helper folder, create functions to select random words

--- app/components/Quiz.js
+++ app/components/Quiz.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import words from 'word-list-json';
+import { selectRandomNumber } from '../helpers/random';
 
 export default class Quiz extends Component {
   constructor() {
@@ -8,7 +10,15 @@ export default class Quiz extends Component {
   render() {
     return (
       <div className="quiz-container">
-        <button className="start-button">Start</button>
+        <button
+          className="start-button"
+          onClick={() => {
+            let random = selectRandomNumber(words.length);
+            console.log(words[random]);
+          }}
+        >
+          Start
+        </button>
       </div>
     );
   }
---
+++ app/helpers/random.js
@@ -0,0 +1,5 @@
+export const selectRandomNumber = max => {
+  return Math.round(Math.random() * max);
+};
+
+export const selectRandomWord = words => {};
