604cdbdb2e4635156f52e05da6c15bf0be70eaed
Tyler Hevia <tylerhevia@gmail.com>
2017-12-31 23:51:29 -0500

    create components folder, create App component

--- app/main.js
+++ app/main.js
@@ -1,1 +1,5 @@
-console.log('Inside app folder');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
