c797c9a82f0db168dec53128cf4a4d40e1b37923
Tyler Hevia <tylerhevia@gmail.com>
2017-12-31 23:45:26 -0500

    update webpack config with babel loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,18 @@
 module.exports = {
-  entry: ['./app/main.js'],
+  entry: './app/main.js',
   output: {
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    port: 3000
   }
 };
